f69b0c5e58869792ad9f01b5914ec4d0d5723453
markusphil <m.pilipp1991@web.de>
2019-04-29 18:59:58 +0200

    update Spinner

--- react-frontend/src/components/core/Spinner.js
+++ react-frontend/src/components/core/Spinner.js
@@ -1,17 +1,10 @@
-import React from 'react';
-
-import './Spinner.css'
+import React from "react";
 
 export const Spinner = () => (
-    <div className="sk-cube-grid">
-    <div className="sk-cube sk-cube1"></div>
-    <div className="sk-cube sk-cube2"></div>
-    <div className="sk-cube sk-cube3"></div>
-    <div className="sk-cube sk-cube4"></div>
-    <div className="sk-cube sk-cube5"></div>
-    <div className="sk-cube sk-cube6"></div>
-    <div className="sk-cube sk-cube7"></div>
-    <div className="sk-cube sk-cube8"></div>
-    <div className="sk-cube sk-cube9"></div>
-    </div>
+  <div className="lds-ring">
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
 );
